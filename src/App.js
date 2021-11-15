bb83fd2ad5dd0a33661997fd9dcb0d6059ee44e0
Chamunorwa Mutezva <ckmutezva@gmail.com>
2021-11-15 04:24:38 +0200

    feat: header btn an logo styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./sass/App.scss"
+import logo from "./assets/shared/logo.svg"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Gallery</h1>
+    <div className="App container bg-white">
+      <header className="flex">
+        <div className="image-holder">
+          <img class="logo" src={logo} alt="" aria-hidden="true" />
+        </div>
+        <button className="btn uppercase fs-100 text-med-grey">
+          Start slideshow
+        </button>
       </header>
     </div>
   );
