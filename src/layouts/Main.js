180c5b20dd1481e7ef4dbc0f7d7b9e9d21f35f89
Chamunorwa Mutezva <ckmutezva@gmail.com>
2021-11-17 21:48:14 +0200

    masonry layout using masonry library

--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -1,5 +1,6 @@
 import { useContext } from "react"
 import { DataContext } from "../context/DataContext"
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry"
 
 const Main = () => {
 
@@ -7,21 +8,24 @@ const Main = () => {
     console.log(data)
 
     return (
-        <main>
-            <h1>main page</h1>
-            <div className="flex image-container">
-                {data.map(elm => {
-                    return <a className="anchor-color" href="./" key={elm.name}>
-                        <figure className="relative">
-                            <img src={elm.images.thumbnail} alt="" />
-                            <figcaption className="absolute">
-                                <h2 className="fs-900">{elm.name}</h2>
-                                 <h3 className="fs-400">{elm.artist.name}</h3>
-                            </figcaption>
-                        </figure>
-                    </a>
-                })}
-            </div>
+        <main className="main">
+            <h1 className="sr-only">galleria photo center - home of the most acclaimed pictures</h1>
+            <ResponsiveMasonry columnsCountBreakPoints={{ 310: 1, 620: 2, 930: 3 , 1240: 4 }} >
+                <Masonry gutter={`1rem`}>
+                    {data.map(elm => {
+                        return <a className="anchor-color link-card flex-2" href="./" key={elm.name} >
+                            <figure className="relative">
+                                <img className="anchor-img" src={elm.images.thumbnail} alt="" />
+                                <figcaption className="absolute">
+                                    <h2 className="fs-900">{elm.name}</h2>
+                                    <h3 className="fs-400">{elm.artist.name}</h3>
+                                </figcaption>
+                            </figure>
+                        </a>
+
+                    })}
+                </Masonry>
+            </ResponsiveMasonry>
         </main>
     )
 }
