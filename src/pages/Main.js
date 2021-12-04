9b11ec1a2382e26728afbb2a0521ddb910a7128c
Chamunorwa Mutezva <ckmutezva@gmail.com>
2021-12-04 18:27:24 +0200

    feat: temporary loading before data is loaded

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -6,12 +6,7 @@ import Masonry, { ResponsiveMasonry } from "react-responsive-masonry"
 const Main = () => {
     const mainPage = useRef()
     const cardRef = useRef()
-    const { data, startSlide, slide } = useContext(DataContext)   
-
-    useEffect(() => {
-        mainPage.current.focus()
-    })  
-    
+    const { data, startSlide, slide, isReady } = useContext(DataContext)
 
     useEffect(() => {
         // stop the slideshow when the main page is slected
@@ -21,33 +16,34 @@ const Main = () => {
     })
 
     return (
-        <main className="main container"
-            tabIndex="-1"
-            ref={mainPage}>
-            <h1 className="sr-only">galleria photo center - home of the most acclaimed pictures</h1>
-            <ResponsiveMasonry columnsCountBreakPoints={{ 310: 1, 620: 2, 930: 3, 1100: 4 }} >
-                <Masonry gutter={`1.5rem`}>
-                    {data && data.map(elm => {
-                        return <Link to={`/slides/${elm.id}`}
-                            className="anchor-color link-card flex-2"
-                            key={elm.name}
-                            ref={cardRef}>
-                            <figure className="relative">
-                                <img className="anchor-img"
-                                    src={elm.images.thumbnail}
-                                    alt="" />
-                                <figcaption className="absolute">
-                                    <h2 className="fs-900 fw-bold">{elm.name}</h2>
-                                    <h3 className="fs-400 darken-color">{elm.artist.name}</h3>
-                                </figcaption>
-                            </figure>
-                        </Link>
+        !isReady() ? <div>Loading</div> :
+            <main className="main container"
+                tabIndex="-1"
+                ref={mainPage}>
+                <h1 className="sr-only">galleria photo center - home of the most acclaimed pictures</h1>
+                <ResponsiveMasonry columnsCountBreakPoints={{ 310: 1, 620: 2, 930: 3, 1100: 4 }} >
+                    <Masonry gutter={`1.5rem`}>
+                        {data && data.map(elm => {
+                            return <Link to={`/slides/${elm.id}`}
+                                className="anchor-color link-card flex-2"
+                                key={elm.name}
+                                ref={cardRef}>
+                                <figure className="relative">
+                                    <img className="anchor-img"
+                                        src={elm.images.thumbnail}
+                                        alt="" />
+                                    <figcaption className="absolute">
+                                        <h2 className="fs-900 fw-bold">{elm.name}</h2>
+                                        <h3 className="fs-400 darken-color">{elm.artist.name}</h3>
+                                    </figcaption>
+                                </figure>
+                            </Link>
 
-                    })}
+                        })}
 
-                </Masonry>
-            </ResponsiveMasonry>
-        </main>
+                    </Masonry>
+                </ResponsiveMasonry>
+            </main>
     )
 }
 export default Main
--- src/pages/Slides.js
+++ src/pages/Slides.js
@@ -8,8 +8,8 @@ import Back from "../assets/shared/icon-back-button.svg"
 const Slides = () => {
 
     const slidePage = useRef()
-    const currentSlide = useRef()  
-    
+    const currentSlide = useRef()
+
     const { getOne, startSlide, slide, data, isReady } = useContext(DataContext)
     const dataLength = data.length
     const [showImage, setShowImage] = useState(false)
@@ -55,11 +55,10 @@ const Slides = () => {
     let nextSlide = null
     let previousSlide = null
     const targetObj = getOne(id);
-    console.log(isReady)
-    console.log(targetObj)
+    
     if (isReady()) {
         nextSlide = targetObj.id >= 15 ? 1 : targetObj.id + 1;
-        previousSlide = targetObj.id > 1 ? targetObj.id - 1 : null;            
+        previousSlide = targetObj.id > 1 ? targetObj.id - 1 : null;
     }
 
     /* ---------------SLIDE PRESENTATION--------------------- */
@@ -89,104 +88,104 @@ const Slides = () => {
 
 
     return (
-        !isReady() ? <div>Loading</div> : <div className="slide"
+        !isReady() ? <div>Loading</div> :
+            <div className="slide"
+                tabIndex="-1"
+                ref={slidePage}>
 
-            tabIndex="-1"
-            ref={slidePage}>
+                <main className="main container main-slide">
+                    <h1 className="sr-only">
+                        Additional information for {targetObj.name} the works of {targetObj.artist.name}
+                    </h1>
 
-            <main className="main container main-slide">
-                <h1 className="sr-only">
-                    Additional information for {targetObj.name} the works of {targetObj.artist.name}
-                </h1>
-
-                <div className="image-content"
-                    tabIndex="-1"
-                    ref={currentSlide}>
-                    <div className="main-image relative">
-                        <div className="view-image absolute-top-bottom">
-                            <button className="btn btn-image" onClick={handleShowImage}>
-                                <img src="../assets/shared/icon-view-image.svg" alt="" aria-hidden="true" />
-                                <span>View image</span>
-                            </button>
+                    <div className="image-content"
+                        tabIndex="-1"
+                        ref={currentSlide}>
+                        <div className="main-image relative">
+                            <div className="view-image absolute-top-bottom">
+                                <button className="btn btn-image" onClick={handleShowImage}>
+                                    <img src="../assets/shared/icon-view-image.svg" alt="" aria-hidden="true" />
+                                    <span>View image</span>
+                                </button>
+                            </div>
+                            <picture className="main-image-container">
+                                <source media="(min-width: 680px)"
+                                    srcSet={targetObj.images.hero.large} />
+                                <img className="anchor-img"
+                                    src={targetObj.images.hero.small}
+                                    alt={`${targetObj.name}`} />
+                            </picture>
+                            <div className="absolute">
+                                <h2 className="fs-900 fw-bold slide-image-name">{targetObj.name}</h2>
+                                <h3 className="fs-600 slide-artist-name">{targetObj.artist.name}</h3>
+                            </div>
                         </div>
-                        <picture className="main-image-container">
-                            <source media="(min-width: 680px)"
-                                srcSet={targetObj.images.hero.large} />
-                            <img className="anchor-img"
-                                src={targetObj.images.hero.small}
-                                alt={`${targetObj.name}`} />
-                        </picture>
-                        <div className="absolute">
-                            <h2 className="fs-900 fw-bold slide-image-name">{targetObj.name}</h2>
-                            <h3 className="fs-600 slide-artist-name">{targetObj.artist.name}</h3>
+                        <div className="author-image">
+                            <img src={targetObj.artist.image} alt={`author ${targetObj.artist.name}`} />
                         </div>
                     </div>
-                    <div className="author-image">
-                        <img src={targetObj.artist.image} alt={`author ${targetObj.artist.name}`} />
+
+                    {/******************------------- OVERLAY SECTION------------ **********************/}
+                    <div className={`overlay ${showImage ? "overlay-show" : ""}`}>
+                        <div className="overlay-container">
+                            <button className="btn-close" onClick={handleShowImage}>Close</button>
+                            <img src={targetObj.images.gallery} alt="" />
+                        </div>
                     </div>
-                </div>
+                    {/******************------------- OVERLAY SECTION END------------ **********************/}
 
-                {/******************------------- OVERLAY SECTION------------ **********************/}
-                <div className={`overlay ${showImage ? "overlay-show" : ""}`}>
-                    <div className="overlay-container">
-                        <button className="btn-close" onClick={handleShowImage}>Close</button>
-                        <img src={targetObj.images.gallery} alt="" />
+
+                    {/*************------ ADDITIONAL CONTENT, IMAGE AND LINK TO EXTRA CONTENT------*******/}
+                    <div className="additional-content">
+                        <h2 className="fs-1200 year">{targetObj.year}</h2>
+                        <p className="fs-500 lighten-mid-grey fw-bold description">{targetObj.description}</p>
+                        <div className="container-btn-source">
+                            <a href={targetObj.source}
+                                className="fs-100 uppercase lighten-mid-grey btn-source letter-spacing"
+                                target="_blank"
+                                rel="noreferrer">
+                                Go to source
+                                <span className="sr-only">Wikipedia documents</span>
+                            </a>
+                        </div>
                     </div>
-                </div>
-                {/******************------------- OVERLAY SECTION END------------ **********************/}
+                </main>
+                {/*************------END OF ADDITIONAL CONTENT, IMAGE AND LINK TO EXTRA CONTENT------*******/}
 
 
-                {/*************------ ADDITIONAL CONTENT, IMAGE AND LINK TO EXTRA CONTENT------*******/}
-                <div className="additional-content">
-                    <h2 className="fs-1200 year">{targetObj.year}</h2>
-                    <p className="fs-500 lighten-mid-grey fw-bold description">{targetObj.description}</p>
-                    <div className="container-btn-source">
-                        <a href={targetObj.source}
-                            className="fs-100 uppercase lighten-mid-grey btn-source letter-spacing"
-                            target="_blank"
-                            rel="noreferrer">
-                            Go to source
-                            <span className="sr-only">Wikipedia documents</span>
-                        </a>
+                {/******************------------- FOOTER SECTION------------ **********************/}
+                <footer className="footer">
+                    <div className="progressive-bar">
+                        <span className="progress" style={{ width: `${progressPercent}%` }}></span>
                     </div>
-                </div>
-            </main>
-            {/*************------END OF ADDITIONAL CONTENT, IMAGE AND LINK TO EXTRA CONTENT------*******/}
+                    <div className="footer-slide container">
+                        <div className="footer-heading">
+                            <h2 className="fs-900 fw-bold slide-image-name-footer">{targetObj.name}</h2>
+                            <h3 className="fs-600 slide-artist-name-footer">{targetObj.artist.name}</h3>
+                        </div>
+                        <div className="controls">
+                            <Link to={`/slides/${previousSlide}`} onClick={handleBackProgressBar}
+                                className={`btn btn-back ${previousSlide ? "" : "btn-disabled"}`}
 
+                            >
+                                <span className="sr-only">select previous data</span>
+                                <img src={Back} alt="" />
+                            </Link>
 
-            {/******************------------- FOOTER SECTION------------ **********************/}
-            <footer className="footer">
-                <div className="progressive-bar">
-                    <span className="progress" style={{ width: `${progressPercent}%` }}></span>
-                </div>
-                <div className="footer-slide container">
-                    <div className="footer-heading">
-                        <h2 className="fs-900 fw-bold slide-image-name-footer">{targetObj.name}</h2>
-                        <h3 className="fs-600 slide-artist-name-footer">{targetObj.artist.name}</h3>
+                            <Link to={`/slides/${nextSlide}`} onClick={handleNextProgressBar}
+                                className={`btn btn-next ${nextSlide ? "" : "btn-disabled"}`}
+                            >
+
+                                <span className="sr-only">select next data</span>
+                                <img src={Next} alt="" />
+                            </Link>
+                        </div>
                     </div>
-                    <div className="controls">
-                        <Link to={`/slides/${previousSlide}`} onClick={handleBackProgressBar}
-                            className={`btn btn-back ${previousSlide ? "" : "btn-disabled"}`}
+                </footer>
 
-                        >
-                            <span className="sr-only">select previous data</span>
-                            <img src={Back} alt="" />
-                        </Link>
+                {/******************------------- FOOTER SECTION END------------ **********************/}
 
-                        <Link to={`/slides/${nextSlide}`} onClick={handleNextProgressBar}
-                            className={`btn btn-next ${nextSlide ? "" : "btn-disabled"}`}
-                        >
-
-                            <span className="sr-only">select next data</span>
-                            <img src={Next} alt="" />
-                        </Link>
-                    </div>
-                </div>
-            </footer>
-
-            {/******************------------- FOOTER SECTION END------------ **********************/}
-
-        </div>
+            </div>
     )
 }
 export default Slides
